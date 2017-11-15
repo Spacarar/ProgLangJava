5889cec7a64861217c7f767ebc4c7edba9df6646
Shawn Pacarar <spacarar@hotmail.com>
2017-11-15 12:00:52 -0700

    Created Worker with threads

--- src/proglangjava/App.java
+++ src/proglangjava/App.java
@@ -5,6 +5,8 @@
  */
 package proglangjava;
 
+import java.util.Random;
+
 /**
  *
  * @author Wolfwood
@@ -15,9 +17,23 @@ public class App {
         App app = new App();
         app.run();
     }
-    
+   Job[] createJobList(){
+       int numJobs = 10;
+        String[] actions ={"Sweeping","Mopping","Doing the dishes","Taking the trash out","Cleaning the windows","Organizing the boxes","taking a break"};
+        Random r= new Random();
+        Job[] joblist=new Job[numJobs];
+        for(int i=0;i<numJobs;i++){
+           joblist[i]=new Job(actions[r.nextInt(7)],r.nextInt(1500));
+        }
+        return joblist;
+   }
     void run() throws Exception{
-        
+        Job[] joblist= createJobList();
+        Thread bob = new Thread(new Worker("Bob",joblist));
+        joblist=createJobList();
+        Thread tina = new Thread(new Worker("Tina",joblist));
+        bob.start();
+        tina.start();
     }
     
 }
